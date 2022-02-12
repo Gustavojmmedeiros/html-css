ac7941a48a1941895cc7e009519cbf126e2a45b2
Gustavo Medeiros <gustavojmmartins@outlook.com>
2022-02-12 14:59:37 -0300

    Penúltima versão

--- html-css-alura/JavaScript para Web - aluramidi/main.js
+++ html-css-alura/JavaScript para Web - aluramidi/main.js
@@ -1,20 +1,40 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play()
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio).play()
+
+    if(elemento === null) {
+        console.log('Elemento não encontrado')
+    }
+
+    if(elemento != null) {
+        console.log(elemento.localName)
+    }
+    // elemento.play()
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla')
 
-let contador = 0
 
-while(contador < listaDeTeclas.length) {
+for(let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas
     const instrumento = tecla[contador].classList[1] //usa-se o classList para acessar a lista e pegar a segunda classe dos botões
-
     const idAudio = `#som_${instrumento}`
 
     tecla[contador].onclick = function() {
         tocaSom(idAudio)
     }
 
-    contador += 1
+    tecla.onkeydown = function(evento) {
+        
+        console.log(evento.code)
+
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa')
+        }
+        
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa')
+    }
+
 }
